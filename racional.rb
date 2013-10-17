abef6759ff6d8868f3803ea805cb5c7b6b549ee1
Manuel Perez <alu0100697698@ull.edu.es>
2013-10-17 15:48:13 +0100

    Practica terminada

--- racional.rb
+++ racional.rb
@@ -3,4 +3,82 @@
 require "./gcd.rb"
 
 class Fraccion
+	
+	attr_reader :a , :b
+	
+	def initialize(a, b)
+		if(b == 0)
+			puts "El denominador no puede ser un 0"
+		else
+			@a, @b = a, b
+			mcd = gcd(a, b)
+			@a = a/mcd
+			@b = b/mcd
+		end
+	end
+	
+	def to_s
+		"#{@a} / #{@b}"
+	end
+	
+	def suma(n, d)
+		if(@b == d)
+			ns = @a + n
+			ds = @b
+		else
+			na = @a * d
+			nb = @b * d
+			nn = n * @b
+			nd = d * @b
+			ns = na + nn
+			ds = nd
+		end
+			mcd = gcd(ns, ds)
+			puts "El resultado de la suma es: #{ns/mcd} / #{ds/mcd}"
+	end
+	
+	def resta(n, d)
+		if(@b == d)
+			nr = @a - n
+			dr = @b
+		else
+			na = @a * d
+			nb = @b * d
+			nn = n * @b
+			nd = d * @b
+			nr = na - nn
+			dr = nd
+		end
+			mcd = gcd(nr, dr)
+			puts "El resultado de la resta es: #{nr/mcd} / #{dr/mcd}"
+	end
+	
+	def producto(n, d)
+		np = @a * n
+		dp = @b * d
+		
+		mcd = gcd(np, dp)
+		puts "El resultado del producto es: #{np/mcd} / #{dp/mcd}"		
+	end
+	
+	def division(n, d)
+		nd = @a * d
+		dd = @b * n
+		
+		mcd = gcd(nd, dd)
+		puts "El resultado de la division es: #{nd/mcd} / #{dd/mcd}"
+	end
+	
 end
+
+f1 = Fraccion.new(4, 2)
+puts f1
+
+f2 = f1.suma(4, 2)
+
+f3 = f1.resta(4, 2)
+
+f4 = f1.producto(4, 2)
+
+f5 = f1.division(4, 2)
+
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,17 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+	def test_simple
+		assert_equal("1 / 1", Fraccion.new(2, 2).to_s)
+		assert_equal("3 / 4", Fraccion.new(3, 4).to_s)
+	end
+		
+	def test_type_check
+		assert_raise(RuntimeError) {Fraccion.new('l', 'pp')}
+	end
+	
+	def test_failure
+		assert_equal("5 / 5", (Fraccion.new(0, 0).producto(5, 5)).to_s)
+	end
 
 end
